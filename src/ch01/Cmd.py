eaf647fde6bb19f3349fa279732a9cd50d9a9d93
Hurf <huruifeng1202@163.com>
2018-10-15 10:30:02 +0800

    重构命令行解析

--- src/ch01/Cmd.py
+++ src/ch01/Cmd.py
@@ -1,33 +1,24 @@
 from ch01.ArgList import ArgList
 
 class Cmd():
-    helpFlag = False
+
     versionFlag = False
     cpOption = ""
     className = ""
     args = []
 
-    def __init__(self, argvs):
-        if len(argvs) > 1:
-            if argvs[1] in {"-help", "?"}:
-                self.printUsage(argvs[0])
-                self.helpFlag = True
-            elif argvs[1] in {"-version", "-v"}:
-                self.printVersion()
-                self.versionFlag = True
-            elif argvs[1] == "-cp":
-                self.cpOption = argvs[2] if not argvs[2] else ""
-                self.className = argvs[3] if not argvs[3] else ""
-                self.args = ArgList(argvs[4:]) if not argvs[4] else []
-                self.printClasspath()
-        else:
-            self.printUsage(argvs[0])
+    def __init__(self, options, args):
+        if options.versionFlag:
+            self.__printVersion()
+        elif options.cpOption:
+            self.cpOption = options.cpOption
+            self.className = args[0]
+            self.args = ArgList(args[1:]) if args[1:] else []
+            self.__printClasspath()
 
-    def printUsage(self, argv):
-        print("Usage:{0} [-options] class [args...]".format(argv))
 
-    def printVersion(self):
+    def __printVersion(self):
         print("version 0.0.1")
 
-    def printClasspath(self):
+    def __printClasspath(self):
         print("classpath:{0} class:{1} args:{2}\n".format(self.cpOption, self.className, self.args))
--- src/ch01/main.py
+++ src/ch01/main.py
@@ -1,7 +1,13 @@
-import sys
+from optparse import OptionParser
 from ch01.Cmd import Cmd
 
-def main():
-    Cmd(sys.argv)
+parser = OptionParser(usage="%prog [-options] class [args...]")
 
-main()
+parser.add_option("-v", "--version", action="store_true", default=False, dest="versionFlag", help="print version and exit.")
+parser.add_option("--cp", action="store", type="string", dest="cpOption", help="classpath")
+parser.add_option("--classpath", action="store", type="string", dest="cpOption", help="classpath")
+(options, args) = parser.parse_args()
+if options:
+    cmd = Cmd(options, args)
+
+
