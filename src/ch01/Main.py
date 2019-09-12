01e62acceb98ec06d75ac18abd60a4b1ac5c0d72
Hurf <huruifeng1202@163.com>
2019-09-12 10:34:34 +0800

    第1章：修改测试函数

--- src/ch01/Main.py
+++ src/ch01/Main.py
@@ -13,18 +13,31 @@ from Cmd import Cmd
 
 
 def main(input_args):
+    # 设置传入参数
     parser = OptionParser(usage="usage:%prog [-options] class [args...]")
 
     parser.add_option("-v", "--version", action="store_true", default=False, dest="versionFlag",
                       help="print version and exit.")
     parser.add_option("--cp", action="store", type="string", dest="cpOption", help="classpath")
     parser.add_option("--classpath", action="store", type="string", dest="cpOption", help="classpath")
+    # 解析参数
     (options, args) = parser.parse_args(input_args)
     if options:
         cmd = Cmd(options, args)
-        cmd.print_classpath()
+        # 启动JVM
+        start_JVM(cmd)
+
+
+# 启动JVM函数，暂时只是打印一些信息而已
+def start_JVM(cmd):
+    cmd.print_classpath()
 
 
 if __name__ == '__main__':
+    # 打印帮助
+    # fake_args = ['-h']
+    # main(fake_args)
+
+    # 使用输入参数测试
     fake_args = ['--cp', 'foo/bar', 'MyApp', 'arg1', 'arg2']
     main(fake_args)
